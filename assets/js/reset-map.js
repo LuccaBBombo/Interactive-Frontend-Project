f50260225298f3d5958ba1cd91804f53fdc3a9dc
Lucca Bombo <lucca.bombo@hotmail.com>
2019-04-24 18:01:37 +0000

    reset-map is now able to reset the map to world view

--- assets/js/reset-map.js
+++ assets/js/reset-map.js
@@ -1,5 +1,19 @@
-function resetMap(){
-    
+function resetMap() {
+
     document.getElementById("locationField").reset();
-    
+
+    var country = document.getElementById('country').value;
+    if (country == 'all') {
+        autocomplete.setComponentRestrictions({ 'country': [] });
+        map.setCenter({ lat: 15, lng: 0 });
+        map.setZoom(2);
+    }
+    else {
+        autocomplete.setComponentRestrictions({ 'country': country });
+        map.setCenter(countries[country].center);
+        map.setZoom(countries[country].zoom);
+    }
+    clearResults();
+    clearMarkers();
+
 }
